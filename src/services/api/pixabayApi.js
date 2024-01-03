794705b9cbe06cbb50c4f7bb21266a0f1c650f9d
stchangai <stchangai>
2024-01-04 03:23:49 +0900

    [ADD] environment variables

--- src/services/api/pixabayApi.js
+++ src/services/api/pixabayApi.js
@@ -1,6 +1,7 @@
+const accessToken = process.env.VUE_APP_KEY_TOKEN;
 export async function getImageRandom(axios) {
   const image = await axios.get(
-      `https://api.unsplash.com/photos/random/?client_id=QCpQmMd34xHiP1qm3UlvbOFySNj3GamhpHOCxVwUurg&count=2`);
+      `https://api.unsplash.com/photos/random/?client_id=${accessToken}&count=2`);
   return image;
 }
 
@@ -12,13 +13,13 @@ export async function getImagesByKeywords(axios, keywords, colors) {
     labelColor = '&color='
   }
   const images = await axios.get(
-      `https://api.unsplash.com/search/photos?client_id=QCpQmMd34xHiP1qm3UlvbOFySNj3GamhpHOCxVwUurg&query=` +
+      `https://api.unsplash.com/search/photos?client_id=${accessToken}&query=` +
       keywords + labelColor + colors + `&per_page=20`);
   return images;
 }
 
 export async function GetAllImages(axios, pageCount = 1, orderBy = 'popular') {
   const allImages = await axios.get(
-      `https://api.unsplash.com/photos?client_id=QCpQmMd34xHiP1qm3UlvbOFySNj3GamhpHOCxVwUurg&per_page=12&page=` + pageCount + `&order_by=` + orderBy);
+      `https://api.unsplash.com/photos?client_id=${accessToken}&per_page=12&page=` + pageCount + `&order_by=` + orderBy);
   return allImages.data;
 }
